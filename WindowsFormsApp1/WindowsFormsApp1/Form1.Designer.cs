dea071b513e9e709214d0ca807acc880f8bbc943
doughfeet <kennethblix@gmail.com>
2017-11-22 08:53:30 +0100

    Got the balls to move, can controll the player ball. Need to find out how to color the players ball track..

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.EnemyBall = new System.Windows.Forms.PictureBox();
+            this.PlayerBall = new System.Windows.Forms.PictureBox();
+            this.Canvas.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Canvas
+            // 
+            this.Canvas.Controls.Add(this.PlayerBall);
+            this.Canvas.Controls.Add(this.EnemyBall);
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas.Location = new System.Drawing.Point(0, 0);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(1644, 895);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            // 
+            // EnemyBall
+            // 
+            this.EnemyBall.BackColor = System.Drawing.Color.Black;
+            this.EnemyBall.Location = new System.Drawing.Point(823, 238);
+            this.EnemyBall.Name = "EnemyBall";
+            this.EnemyBall.Size = new System.Drawing.Size(50, 50);
+            this.EnemyBall.TabIndex = 0;
+            this.EnemyBall.TabStop = false;
+            // 
+            // PlayerBall
+            // 
+            this.PlayerBall.BackColor = System.Drawing.SystemColors.Highlight;
+            this.PlayerBall.Location = new System.Drawing.Point(622, 399);
+            this.PlayerBall.Name = "PlayerBall";
+            this.PlayerBall.Size = new System.Drawing.Size(50, 50);
+            this.PlayerBall.TabIndex = 1;
+            this.PlayerBall.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1644, 895);
+            this.Controls.Add(this.Canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Canvas.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.PictureBox PlayerBall;
+        private System.Windows.Forms.PictureBox EnemyBall;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+
+        public int speed_left = 4;
+        public int speed_top = 4;
+        public int point = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+
+            this.TopMost = true;
+
+            PlayerBall.Top = Canvas.Bottom - (Canvas.Bottom / 20);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            EnemyBall.Left += speed_left;
+            EnemyBall.Top += speed_top;
+
+
+            if (EnemyBall.Left <= Canvas.Left && EnemyBall.BackColor != Canvas.BackColor)
+            {
+                speed_left = -speed_left;
+            }
+            if (EnemyBall.Right >= Canvas.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (EnemyBall.Top <= Canvas.Top)
+            {
+                speed_top = -speed_top;
+            }
+            if (EnemyBall.Top >= Canvas.Bottom)
+            {
+                speed_top = -speed_top;
+            }
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Brush brush = (Brush)Brushes.Blue;
+            Graphics g = this.CreateGraphics();
+            X = PlayerBall.Location.X;
+            Y = PlayerBall.Location.Y;
+
+            if (e.KeyCode == Keys.Right && PlayerBall.Right <= Canvas.Right )
+            {
+                X += 4;
+            }
+            else if (e.KeyCode == Keys.Left && PlayerBall.Left >= Canvas.Left)
+            {
+                X -= 4;
+            }
+            else if (e.KeyCode == Keys.Up && PlayerBall.Top >= Canvas.Top)
+            {
+                Y -= 4;
+            }
+            else if (e.KeyCode == Keys.Down && PlayerBall.Bottom <= Canvas.Bottom)
+            {
+                Y += 4;
+            }
+
+            PlayerBall.Location = new Point(X, Y);
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Blue, X, Y, 1, 1);
+        }
+    }
+}
